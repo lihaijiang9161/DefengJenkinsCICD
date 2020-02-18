3438b2c6322720ee18927cdae7599f3b2aacf5b4
lihaijiang9161 <59007046+lihaijiang9161@users.noreply.github.com>
2020-02-18 13:11:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,66 +1,95 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>BJS CodeDeploySample</title>
-  <style>
-    body {
-      color: #ffffff;
-      background-color: #0188cc;
-      font-family: Arial, sans-serif;
-      font-size: 14px;
-    }
-    
-    h1 {
-      font-size: 500%;
-      font-weight: normal;
-      margin-bottom: 0;
-    }
-    
-    h2 {
-      font-size: 200%;
-      font-weight: normal;
-      margin-bottom: 0;
-    }
-  </style>
-</head>
-<body>
-  <div align="center">
-    <h1>AWS DevOps Demo for Disneyland - V222</h1>
-    <h2>Github + ALB + CodeDeploy + Jenkins</h2> 
-    <h2>AWS BJS CodeDeploy Demo</h2>
-  </div>
-  </br>
-  <div align="center">
-  <?php
-    $instance_id = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
-    $zone = @file_get_contents("http://169.254.169.254/latest/meta-data/placement/availability-zone");
-    $private_id = @file_get_contents("http://instance-data/latest/meta-data/local-ipv4");
-  ?>
-    <img src="https://s3.cn-north-1.amazonaws.com.cn/bjsdemo/CodeDeploy-Application-Code/awschina.png" width="300"/>
-    </br>    
-    <h2>EC2 Instance ID: <?php echo $instance_id; ?></h2>
-    <h2>EC2 Zone: <?php echo $zone; ?></h2>
-    <h2>EC2 Private IP: <?php echo $private_id; ?></h2>
-    </br>
-    <p>Please refer this link for more details. <a href="http://aws.amazon.com/documentation/codedeploy">AWS CodeDeploy Document</a>. Blue: #0188cc, Green: #00cc3a</p>
-    <p>danrongm@amazon.com</a>.</p>
-  </div>    
-</body>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo("");
+echo("                          <!DOCTYPE html>");
+echo("<!-- saved from url=(0019)http://www.htdf.cc/ -->");
+echo("<html lang=\"zh\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">");
+echo("  ");
+echo("  <title>华泰德丰</title>");
+echo("  <script type=\"text/javascript\" src=\"./华泰德丰_files/jquery-1.9.1.min.js.下载\"></script>");
+echo("  <script type=\"text/javascript\" src=\"./华泰德丰_files/common-menu.js.下载\"></script>");
+echo("  <!--[if lt IE 9]>");
+echo("        <script src=\"/tpl/htdf/Public/js/html5shiv.min.js\"></script>");
+echo("  <![endif]-->");
+echo("  <link rel=\"stylesheet\" href=\"./华泰德丰_files/main.css\">");
+echo(" </head>");
+echo(" <body>");
+echo("	<header>");
+echo("		<div class=\"sliders\">");
+echo("			<nav>");
+echo("	<div class=\"htdf-header\">");
+echo("		<div class=\"htdf-header-wrap\">");
+echo("			<a href=\"http://www.htdf.cc/\"><div class=\"htdf-header-logo\"></div></a>");
+echo("				<ul id=\"\" class=\"htdf-header-link\"><li class=\"\" id=\"menu-item-1\"><a href=\"http://www.htdf.cc/\" target=\"\" class=\"link cur\"><span>首页</span><i>HOME</i></a></li><li class=\"has-submenu \" id=\"menu-item-2\"><a href=\"http://www.htdf.cc/index.php?m=list&amp;a=index&amp;id=9\" target=\"\" class=\"link\"><span>解决方案</span><i>SOLUTIONS</i></a><ul class=\"menu\"><li class=\"\" id=\"menu-item-6\"><a href=\"http://www.htdf.cc/index.php?m=list&amp;a=index&amp;id=9\" target=\"\">国家局气象业务系统</a></li><li class=\"\" id=\"menu-item-7\"><a href=\"http://www.htdf.cc/index.php?m=list&amp;a=index&amp;id=13\" target=\"\">气象服务系统平台</a></li><li class=\"\" id=\"menu-item-9\"><a href=\"http://www.htdf.cc/index.php?m=list&amp;a=index&amp;id=23\" target=\"\">气象观测业务系统平台</a></li><li class=\"\" id=\"menu-item-8\"><a href=\"http://www.htdf.cc/index.php?m=list&amp;a=index&amp;id=26\" target=\"\">信息处理系统平台</a></li><li class=\"\" id=\"menu-item-14\"><a href=\"http://www.htdf.cc/index.php?m=list&amp;a=index&amp;id=32\" target=\"\">商业气象服务</a></li></ul></li><li class=\"\" id=\"menu-item-3\"><a href=\"http://www.htdf.cc/index.php?m=list&amp;a=index&amp;id=2\" target=\"\" class=\"link\"><span>成功案例</span><i>CASES</i></a></li><li class=\"\" id=\"menu-item-4\"><a href=\"http://www.htdf.cc/index.php?m=list&amp;a=index&amp;id=3\" target=\"\" class=\"link\"><span>公司新闻</span><i>NEWS</i></a></li><li class=\"has-submenu \" id=\"menu-item-13\"><a href=\"javascript:void(0)\" target=\"\" class=\"link\"><span>关于我们</span><i>ABOUT US</i></a><ul class=\"menu\"><li class=\"\" id=\"menu-item-5\"><a href=\"http://www.htdf.cc/index.php?m=page&amp;a=index&amp;id=1\" target=\"\">公司介绍</a></li><li class=\"\" id=\"menu-item-11\"><a href=\"http://www.htdf.cc/index.php?m=page&amp;a=index&amp;id=2\" target=\"\">联系我们</a></li><li class=\"\" id=\"menu-item-12\"><a href=\"http://www.htdf.cc/index.php?m=page&amp;a=index&amp;id=50\" target=\"\">人才招聘</a></li></ul></li></ul>		</div>");
+echo("	</div>");
+echo("</nav>");
+echo("<script>");
+echo("$(\".htdf-header-link a\").each(function(){");
+echo("   if ($(this)[0].href == String(window.location) && $(this).attr(\'href\')!=\"\") {");
+echo("   	$(this).addClass(\"cur\");");
+echo("	}");
+echo("});");
+echo("</script>");
+echo("			<div class=\"slider-wrap\">");
+echo("								<div class=\"slider-1 slider\" style=\"display: none; background-image: url(&quot;/data/upload/5e2efc2fe0b08.jpg&quot;);\"></div><div class=\"slider-2 slider\" style=\"display: block; background-image: url(&quot;/data/upload/5e153a083e2ad.jpg&quot;);\"></div><div class=\"slider-3 slider\" style=\"display: none; background-image: url(&quot;/data/upload/5e032dbe0d6a8.jpg&quot;);\"></div><div class=\"slider-4 slider\" style=\"display: none; background-image: url(&quot;/data/upload/5d721ba75cb61.jpg&quot;);\"></div>			</div>");
+echo("			<div class=\"slider-control\">");
+echo("				<div class=\"slider-control-title\"><span style=\"display: inline-block;\"><a href=\"http://www.htdf.cc/index.php?m=article&amp;a=index&amp;id=121\">国家气象观测实时业务平台介绍</a></span>");
+echo("					<div class=\"slider-btn-wrap\">");
+echo("						<a href=\"javascript:void(0)\" class=\"prev\"></a>");
+echo("						<a href=\"javascript:void(0)\" class=\"next\"></a>");
+echo("					</div>");
+echo("				</div>");
+echo("			</div>");
+echo("		</div>");
+echo("	</header>");
+echo("	<section class=\"body\">");
+echo("		<ul class=\"hotspot clearfix\">");
+echo("			<li class=\"item-1\" onclick=\"javascript:location.href=&#39;/index.php?m=article&amp;a=index&amp;id=123&#39;;\">");
+echo("				<b><i>0</i>1</b>");
+echo("				<h2><a href=\"http://www.htdf.cc/index.php?m=article&amp;a=index&amp;id=123\">武汉加油！中国加油！</a></h2>");
+echo("				<p>新型冠状病毒感染的肺炎疫情牵动全国人心，大家守望相助、众志成城、共克时艰！武汉加油！中国加油！</p>");
+echo("			</li><li class=\"item-2 opcity\" onclick=\"javascript:location.href=&#39;/index.php?m=article&amp;a=index&amp;id=121&#39;;\">");
+echo("				<b><i>0</i>2</b>");
+echo("				<h2><a href=\"http://www.htdf.cc/index.php?m=article&amp;a=index&amp;id=121\">国家气象观测实时业务平台介绍</a></h2>");
+echo("				<p>由我司承建的国家气象观测实时业务平台于2019年12月23日在中国气象局气象探测中心正式启用。</p>");
+echo("			</li><li class=\"item-3\" onclick=\"javascript:location.href=&#39;/index.php?m=article&amp;a=index&amp;id=120&#39;;\">");
+echo("				<b><i>0</i>3</b>");
+echo("				<h2><a href=\"http://www.htdf.cc/index.php?m=article&amp;a=index&amp;id=120\">国家气象观测实时业务平台投入运行</a></h2>");
+echo("				<p>由我司承建的国家气象观测实时业务平台，于2019年12月23日在中国气象局气象探测中心投入运行。</p>");
+echo("			</li><li class=\"item-4\" onclick=\"javascript:location.href=&#39;/index.php?m=article&amp;a=index&amp;id=116&#39;;\">");
+echo("				<b><i>0</i>4</b>");
+echo("				<h2><a href=\"http://www.htdf.cc/index.php?m=article&amp;a=index&amp;id=116\">中国气象服务协会到访探讨“中国气候好产品”运营合作</a></h2>");
+echo("				<p>为推动“中国气候好产品”后续工作的顺利开展，9月4日下午，中国气象服务协会秘书处秘书长屈雅一行到访华泰德丰。</p>");
+echo("			</li>		</ul>");
+echo("		<ul class=\"hotpoint clearfix\">");
+echo("				<li><a href=\"http://www.htdf.cc/solutions.html\" target=\"_self\"><img src=\"./华泰德丰_files/sel1.png\" alt=\"ASOM运行管理系统\">");
+echo("				<div>");
+echo("					<h2>ASOM运行管理系统</h2>");
+echo("					<p>提高省市县三级保障部门的气象技术装备保障能力，为保障气象探测数据的“准确、可靠、连续”提供有力的技术支撑。</p>");
+echo("				</div>");
+echo("				</a>");
+echo("			</li><li><a href=\"http://www.htdf.cc/Portal/list/index/id/10.html\" target=\"_self\"><img src=\"./华泰德丰_files/sel2.png\" alt=\"MDOS\">");
+echo("				<div>");
+echo("					<h2>MDOS</h2>");
+echo("					<p>MDOS是基于中国气象局预报与网络司开展实时历史资料一体化业务，而开发的一套业务系统。</p>");
+echo("				</div>");
+echo("				</a>");
+echo("			</li><li><a href=\"http://www.htdf.cc/Portal/list/index/id/16.html\" target=\"_self\"><img src=\"./华泰德丰_files/sel3.png\" alt=\"公共服务气象\">");
+echo("				<div>");
+echo("					<h2>公共服务气象</h2>");
+echo("					<p>公共平台实现了省、市、县三级的气象服务产品制作、发布、管理、展示的内部业务系统。</p>");
+echo("				</div>");
+echo("				</a>");
+echo("			</li>	");
+echo("		</ul>");
+echo("		<div class=\"foot-bar\"></div>");
+echo("	</section>");
+echo("	<footer class=\"foot body\">");
+echo("	<p>");
+echo("		<img src=\"./华泰德丰_files/link-arrow-logo.gif\">");
+echo("		<span><a href=\"http://www.htdf.cc/contactus.html\">联系我们</a> | <a href=\"http://www.htdf.cc/recruitment.html\">人才招聘</a> | 服务热线：010-84937639</span>");
+echo("		<span class=\"beian\"> 京ICP备14045055号 | 京公网安备11010802024815</span>");
+echo("		<span class=\"copyright\">© 2015 北京华泰德丰技术有限公司 版权所有</span>");
+echo("			</p>");
+echo("</footer>");
+echo("");
+echo(" ");
+echo("</body></html>      ");
