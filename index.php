9b522796ca0d31e3963c51daa45e68f37f3b9aba
lihaijiang <1012876100@qq.com>
2020-02-18 16:00:18 +0800

    第一次提交

---
+++ index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="zh">
+ <head>
+  <meta charset="utf-8">
+  <title>华泰德丰</title>
+  <script type="text/javascript" src="jquery-1.9.1.min.js" tppabs="http://www.htdf.cc/tpl/htdf/Public/js/jquery-1.9.1.min.js"></script>
+  <script type="text/javascript" src="common-menu.js" tppabs="http://www.htdf.cc/tpl/htdf/Public/js/common-menu.js"></script>
+  <!--[if lt IE 9]>
+        <script src="html5shiv.min.js" tppabs="http://www.htdf.cc/tpl/htdf/Public/js/html5shiv.min.js"></script>
+  <![endif]-->
+  <link rel="stylesheet" href="main.css" tppabs="http://www.htdf.cc/tpl/htdf/Public/css/main.css">
+ </head>
+ <body>
+	<header>
+		<div class="sliders">
+			<nav>
+	<div class="htdf-header">
+		<div class="htdf-header-wrap">
+			<a href="index.htm" tppabs="http://www.htdf.cc/"><div class="htdf-header-logo"></div></a>
+				<ul id=""  class="htdf-header-link"><li   class=''   id= 'menu-item-1'><a href="index.htm" tppabs="http://www.htdf.cc/" target='' class='link'><span>首页</span><i>HOME</i></a></li><li   class='has-submenu '   id= 'menu-item-2'><a href="index.php-m=list&a=index&id=9.htm" tppabs="http://www.htdf.cc/index.php?m=list&a=index&id=9" target='' class='link'><span>解决方案</span><i>SOLUTIONS</i></a><ul class='menu'><li   class='' id= 'menu-item-6'><a href="index.php-m=list&a=index&id=9.htm" tppabs="http://www.htdf.cc/index.php?m=list&a=index&id=9" target=''>国家局气象业务系统</a></li><li   class=''   id= 'menu-item-7'><a href="index.php-m=list&a=index&id=13.htm" tppabs="http://www.htdf.cc/index.php?m=list&a=index&id=13" target=''>气象服务系统平台</a></li><li   class=''   id= 'menu-item-9'><a href="index.php-m=list&a=index&id=23.htm" tppabs="http://www.htdf.cc/index.php?m=list&a=index&id=23" target=''>气象观测业务系统平台</a></li><li   class=''   id= 'menu-item-8'><a href="index.php-m=list&a=index&id=26.htm" tppabs="http://www.htdf.cc/index.php?m=list&a=index&id=26" target=''>信息处理系统平台</a></li><li   class=''   id= 'menu-item-14'><a href="index.php-m=list&a=index&id=32.htm" tppabs="http://www.htdf.cc/index.php?m=list&a=index&id=32" target=''>商业气象服务</a></li></ul></li><li   class=''   id= 'menu-item-3'><a href="index.php-m=list&a=index&id=2.htm" tppabs="http://www.htdf.cc/index.php?m=list&a=index&id=2" target='' class='link'><span>成功案例</span><i>CASES</i></a></li><li   class=''   id= 'menu-item-4'><a href="index.php-m=list&a=index&id=3.htm" tppabs="http://www.htdf.cc/index.php?m=list&a=index&id=3" target='' class='link'><span>公司新闻</span><i>NEWS</i></a></li><li   class='has-submenu '   id= 'menu-item-13'><a href='javascript:void(0)' target='' class='link'><span>关于我们</span><i>ABOUT US</i></a><ul class='menu'><li   class='' id= 'menu-item-5'><a href="index.php-m=page&a=index&id=1.htm" tppabs="http://www.htdf.cc/index.php?m=page&a=index&id=1" target=''>公司介绍</a></li><li   class=''   id= 'menu-item-11'><a href="index.php-m=page&a=index&id=2.htm" tppabs="http://www.htdf.cc/index.php?m=page&a=index&id=2" target=''>联系我们</a></li><li   class=''   id= 'menu-item-12'><a href="index.php-m=page&a=index&id=50.htm" tppabs="http://www.htdf.cc/index.php?m=page&a=index&id=50" target=''>人才招聘</a></li></ul></li></ul>		</div>
+	</div>
+</nav>
+<script>
+$(".htdf-header-link a").each(function(){
+   if ($(this)[0].href == String(window.location) && $(this).attr('href')!="") {
+   	$(this).addClass("cur");
+	}
+});
+</script>
+			<div class="slider-wrap">
+								<div class="slider-1 slider" style="display:block; background-image:url(5e2efc2fe0b08.jpg)/*tpa=http://www.htdf.cc/data/upload/5e2efc2fe0b08.jpg*/;"></div><div class="slider-2 slider" style="display:block; background-image:url(5e153a083e2ad.jpg)/*tpa=http://www.htdf.cc/data/upload/5e153a083e2ad.jpg*/;"></div><div class="slider-3 slider" style="display:block; background-image:url(5e032dbe0d6a8.jpg)/*tpa=http://www.htdf.cc/data/upload/5e032dbe0d6a8.jpg*/;"></div><div class="slider-4 slider" style="display:block; background-image:url(5d721ba75cb61.jpg)/*tpa=http://www.htdf.cc/data/upload/5d721ba75cb61.jpg*/;"></div>			</div>
+			<div class="slider-control">
+				<div class="slider-control-title"><span></span>
+					<div class="slider-btn-wrap">
+						<a href="javascript:void(0)" class="prev"></a>
+						<a href="javascript:void(0)" class="next"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<section class="body">
+		<ul class="hotspot clearfix">
+			<li class="item-1" onclick="javascript:location.href='index.php-m=article&a=index&id=123.htm'/*tpa=http://www.htdf.cc/index.php?m=article&a=index&id=123*/;">
+				<b><i>0</i>1</b>
+				<h2><a href="index.php-m=article&a=index&id=123.htm" tppabs="http://www.htdf.cc/index.php?m=article&a=index&id=123">武汉加油！中国加油！</a></h2>
+				<p>新型冠状病毒感染的肺炎疫情牵动全国人心，大家守望相助、众志成城、共克时艰！武汉加油！中国加油！</p>
+			</li><li class="item-2" onclick="javascript:location.href='index.php-m=article&a=index&id=121.htm'/*tpa=http://www.htdf.cc/index.php?m=article&a=index&id=121*/;">
+				<b><i>0</i>2</b>
+				<h2><a href="index.php-m=article&a=index&id=121.htm" tppabs="http://www.htdf.cc/index.php?m=article&a=index&id=121">国家气象观测实时业务平台介绍</a></h2>
+				<p>由我司承建的国家气象观测实时业务平台于2019年12月23日在中国气象局气象探测中心正式启用。</p>
+			</li><li class="item-3" onclick="javascript:location.href='index.php-m=article&a=index&id=120.htm'/*tpa=http://www.htdf.cc/index.php?m=article&a=index&id=120*/;">
+				<b><i>0</i>3</b>
+				<h2><a href="index.php-m=article&a=index&id=120.htm" tppabs="http://www.htdf.cc/index.php?m=article&a=index&id=120">国家气象观测实时业务平台投入运行</a></h2>
+				<p>由我司承建的国家气象观测实时业务平台，于2019年12月23日在中国气象局气象探测中心投入运行。</p>
+			</li><li class="item-4" onclick="javascript:location.href='index.php-m=article&a=index&id=116.htm'/*tpa=http://www.htdf.cc/index.php?m=article&a=index&id=116*/;">
+				<b><i>0</i>4</b>
+				<h2><a href="index.php-m=article&a=index&id=116.htm" tppabs="http://www.htdf.cc/index.php?m=article&a=index&id=116">中国气象服务协会到访探讨“中国气候好产品”运营合作</a></h2>
+				<p>为推动“中国气候好产品”后续工作的顺利开展，9月4日下午，中国气象服务协会秘书处秘书长屈雅一行到访华泰德丰。</p>
+			</li>		</ul>
+		<ul class="hotpoint clearfix">
+				<li><a href="solutions.html" tppabs="http://www.htdf.cc/solutions.html" target="_self"><img src="sel1.png" tppabs="http://www.htdf.cc/tpl/htdf/Public/images/sel1.png" alt="ASOM运行管理系统">
+				<div>
+					<h2>ASOM运行管理系统</h2>
+					<p>提高省市县三级保障部门的气象技术装备保障能力，为保障气象探测数据的“准确、可靠、连续”提供有力的技术支撑。</p>
+				</div>
+				</a>
+			</li><li><a href="10.html" tppabs="http://www.htdf.cc/Portal/list/index/id/10.html" target="_self"><img src="sel2.png" tppabs="http://www.htdf.cc/tpl/htdf/Public/images/sel2.png" alt="MDOS">
+				<div>
+					<h2>MDOS</h2>
+					<p>MDOS是基于中国气象局预报与网络司开展实时历史资料一体化业务，而开发的一套业务系统。</p>
+				</div>
+				</a>
+			</li><li><a href="16.html" tppabs="http://www.htdf.cc/Portal/list/index/id/16.html" target="_self"><img src="sel3.png" tppabs="http://www.htdf.cc/tpl/htdf/Public/images/sel3.png" alt="公共服务气象">
+				<div>
+					<h2>公共服务气象</h2>
+					<p>公共平台实现了省、市、县三级的气象服务产品制作、发布、管理、展示的内部业务系统。</p>
+				</div>
+				</a>
+			</li>	
+		</ul>
+		<div class="foot-bar"></div>
+	</section>
+	<footer class="foot body">
+	<p>
+		<img src="link-arrow-logo.gif" tppabs="http://www.htdf.cc/tpl/htdf/Public/images/link-arrow-logo.gif">
+		<span><a href="contactus.html" tppabs="http://www.htdf.cc/contactus.html">联系我们</a> | <a href="recruitment.html" tppabs="http://www.htdf.cc/recruitment.html">人才招聘</a> | 服务热线：010-84937639</span>
+		<span class="beian"> 京ICP备14045055号 | 京公网安备11010802024815</span>
+		<span class="copyright">© 2015 北京华泰德丰技术有限公司 版权所有</span>
+			</p>
+</footer>
+
+ </body>
+</html>
